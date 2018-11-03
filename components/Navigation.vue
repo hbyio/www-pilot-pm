0eb01e3d09c167cc161a6e32f4ca24bbc678c3cd
Matthieu Bellon <matthieubellon@gmail.com>
2018-11-03 09:05:27 +0100

    Scrolling localized almost OK

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -16,8 +16,8 @@
       <div id="navbarBasicExample" class="navbar-menu">
 
         <div class="navbar-start">
-          <nuxt-link class="navbar-item is-tab" :to="localePath({name:'index', hash:'#functionality-section'},$i18n.locale)">{{ $t('features') }}</nuxt-link>
-          <nuxt-link class="navbar-item is-tab" :to="localePath({name:'index', hash:'#prices-section'},$i18n.locale)">{{ $t('prices') }}</nuxt-link>
+          <nuxt-link class="navbar-item is-tab" :to="localePath({name:'index', hash:'#functionality-section'},$i18n.locale)" v-scroll-to="'#functionality-section'">{{ $t('features') }}</nuxt-link>
+          <nuxt-link class="navbar-item is-tab" :to="localePath({name:'index', hash:'#prices-section'},$i18n.locale)" v-scroll-to="'#prices-section'">{{ $t('prices') }}</nuxt-link>
           <nuxt-link class="navbar-item is-tab" :to="localePath({name:'resources'}, $i18n.locale)">{{ $t('resources') }}</nuxt-link>
         </div>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
-const pkg = require('./package')
+const pkg = require("./package");
 
 module.exports = {
-  mode: 'spa',
+  mode: "spa",
 
   /*
   ** Headers of the page
@@ -9,33 +9,31 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     bodyAttrs: {
-      class: 'homepage'
+      class: "homepage"
     },
-    htmlAttrs:{
-      class: 'has-navbar-fixed-top'
+    htmlAttrs: {
+      class: "has-navbar-fixed-top"
     }
   },
 
   loadingIndicator: {
-    name: 'circle',
-    color: '#3B8070',
-    background: 'white'
+    name: "circle",
+    color: "#3B8070",
+    background: "white"
   },
 
   /*
   ** Customize the progress-bar color
   */
- loading: {
-    color: 'blue',
-    height: '3px'
+  loading: {
+    color: "blue",
+    height: "3px"
   },
 
   /*
@@ -44,50 +42,49 @@ module.exports = {
   css: [
     // Load a Node.js module directly (here it's a Sass file)
     // 'bulma',
-    '@/assets/scss/main.scss',
+    "@/assets/scss/main.scss"
   ],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-    '~plugins/vue-scrollto.js'
-  ],
+  plugins: ["~plugins/vue-scrollto.js"],
 
   router: {
-    scrollBehavior: function (to, from, savedPosition) {
+    scrollBehavior: function(to, from, savedPosition) {
       // if the returned position is falsy or an empty object,
       // will retain current scroll position.
-      let position = false
+      let position = false;
 
       // if no children detected
       if (to.matched.length < 2) {
         // scroll to the top of the page
-        position = { x: 0, y: 0 }
-      } else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
+        position = { x: 0, y: 0 };
+      } else if (
+        to.matched.some(r => r.components.default.options.scrollToTop)
+      ) {
         // if one of the children has scrollToTop option set to true
-        position = { x: 0, y: 0 }
+        position = { x: 0, y: 0 };
       }
 
       // savedPosition is only available for popstate navigations (back button)
       if (savedPosition) {
-        position = savedPosition
+        position = savedPosition;
       }
 
       return new Promise(resolve => {
         // wait for the out transition to complete (if necessary)
-        window.$nuxt.$once('triggerScroll', () => {
+        window.$nuxt.$once("triggerScroll", () => {
           // coords will be used if no selector is provided,
           // or if the selector didn't match any element.
           if (to.hash && document.querySelector(to.hash)) {
             // scroll to anchor by returning the selector
-            position = { selector: to.hash }
+            position = { selector: to.hash };
           }
-          resolve(position)
-        })
-      })
+          resolve(position);
+        });
+      });
     }
-    
   },
 
   /*
@@ -95,41 +92,48 @@ module.exports = {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
     //'@nuxtjs/bulma',
-    ['nuxt-i18n', {
-      locales: [
-        {
-          code: 'en',
-          name: 'English',
-          iso: 'en-US',
-        },
-        {
-          code: 'fr',
-          name: 'Français',
-          iso: 'fr-FR',
-        }
-      ],
-      defaultLocale: 'en',
-      vueI18nLoader: true,
-      baseUrl: 'https://www.pilot.pm',
-      vueI18n: {
-        fallbackLocale: 'en',
-        messages: {
-          en: {
-            features: 'features',
-            prices:'prices',
-            resources:'resources'
+    [
+      "nuxt-i18n",
+      {
+        locales: [
+          {
+            code: "en",
+            name: "English",
+            iso: "en-US"
           },
-          fr: {
-            features: 'fonctionnalités',
-            prices:'tarifs',
-            resources:'ressources'
+          {
+            code: "fr",
+            name: "Français",
+            iso: "fr-FR"
+          }
+        ],
+        defaultLocale: "en",
+        vueI18nLoader: true,
+        baseUrl: "https://www.pilot.pm",
+        vueI18n: {
+          fallbackLocale: "en",
+          messages: {
+            en: {
+              features: "features",
+              prices: "prices",
+              resources: "resources",
+              signup: "Signup",
+              login: "Login"
+            },
+            fr: {
+              features: "fonctionnalités",
+              prices: "tarifs",
+              resources: "ressources",
+              signup: "Essai gratuit",
+              login: "Se connecter"
+            }
           }
         }
       }
-    }],
+    ]
   ],
   /*
   ** Axios module configuration
@@ -156,12 +160,12 @@ module.exports = {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
