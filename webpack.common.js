c31c0bd6256fc91152ffff7900d4b71d997cd0e6
Matthieu Bellon <matthieubellon@gmail.com>
2019-04-29 17:53:04 +0200

    add Victor hugo

--- webpack.common.js
+++ webpack.common.js
@@ -36,11 +36,6 @@ module.exports = {
       }
     ]
   },
-  resolve: {
-    alias: {
-      "vue$": "vue/dist/vue.esm.js" // 'vue/dist/vue.common.js' for webpack 1
-    }
-  },
 
   plugins: [
     new webpack.ProvidePlugin({
