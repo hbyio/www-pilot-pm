f0d13c26151d7f03759bdb7de058ce3e79fcc2c5
Matthieu <matt@pilot.pm>
2021-01-04 15:12:35 +0100

    Deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,19 @@ const colors = require("tailwindcss/colors")
 module.exports = {
     // Warning /!\ Purge is called only with NODE_ENV variable set to 'production'
     purge: {
-        enabled: process.env.HUGO_ENVIRONMENT === "production",
+        enabled: process.env.HUGO_ENV === "production",
         mode: "all",
         preserveHtmlElements: false,
-        content: ["./layouts/**/*.html", "./content/**/*.md", "./layouts/**/*.svg"],
+        content: ["./hugo_stats.json"],
         // or content: [ './hugo_stats.json' ], if you write stats at build (https://github.com/bep/hugo-starter-tailwind-basic/blob/master/tailwind.config.js)
+        options: {
+            //whitelist: [ 'pl-1', 'pl-3' ],
+            defaultExtractor: (content) => {
+                let els = JSON.parse(content).htmlElements
+                els = els.tags.concat(els.classes, els.ids)
+                return els
+            },
+        },
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
